2e08ed2d62539dcbddb76d158eb6ada192c24728
DanielGamboa-10 <danieljgamboa55@gmail.com>
2026-08-15 19:05:11 -0500

    ⚙️ feat: sistema de configuración y reglajes del monoplaza (modos, aero, neumáticos y combustible)

---
+++ demo/src/main/java/com/formula1/service/ConfiguracionService.java
@@ -0,0 +1,51 @@
+package com.formula1.service;
+
+import com.formula1.data.DataStore;
+import com.formula1.model.*;
+
+import java.util.*;
+
+public class ConfiguracionService {
+    private final DataStore store;
+    private ConfiguracionVehiculo configuracionActual;
+
+    public ConfiguracionService() {
+        this.store = DataStore.getInstance();
+        // Por defecto
+        this.configuracionActual = new ConfiguracionVehiculo(
+                ModoConduccion.NORMAL,
+                CargaAerodinamica.MEDIA,
+                PresionNeumaticos.ESTANDAR,
+                EstrategiaCombustible.BALANCEADA
+        );
+    }
+
+    public ConfiguracionVehiculo getConfiguracionActual() {
+        return configuracionActual;
+    }
+
+    public void setConfiguracionActual(ConfiguracionVehiculo configuracionActual) {
+        this.configuracionActual = configuracionActual;
+    }
+
+    public void guardarConfiguracion(String nombre, ConfiguracionVehiculo config) {
+        store.getConfiguracionesGuardadas().put(nombre, new ConfiguracionVehiculo(
+                config.getModoConduccion(),
+                config.getCargaAerodinamica(),
+                config.getPresionNeumaticos(),
+                config.getEstrategiaCombustible()
+        ));
+    }
+
+    public Map<String, ConfiguracionVehiculo> listarConfiguracionesGuardadas() {
+        return Collections.unmodifiableMap(store.getConfiguracionesGuardadas());
+    }
+
+    public Optional<ConfiguracionVehiculo> cargarConfiguracion(String nombre) {
+        return Optional.ofNullable(store.getConfiguracionesGuardadas().get(nombre));
+    }
+
+    public boolean eliminarConfiguracion(String nombre) {
+        return store.getConfiguracionesGuardadas().remove(nombre) != null;
+    }
+}
